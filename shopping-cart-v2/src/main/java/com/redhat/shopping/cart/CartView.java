a78773f5ba85d6f73eb5720549cc581c6674ff67
Sergio Abarca <48446450+sergioab1989@users.noreply.github.com>
2026-03-11 12:36:10 -0300

    Added snapshot
    Added a snapshot field to store the timestamp of the cart view creation.

--- shopping-cart-v2/src/main/java/com/redhat/shopping/cart/CartView.java
+++ shopping-cart-v2/src/main/java/com/redhat/shopping/cart/CartView.java
@@ -1,5 +1,6 @@
 package com.redhat.shopping.cart;
 
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Collection;
 
@@ -7,12 +8,14 @@ public class CartView {
 
     public ArrayList<CartItem> products;
     public int totalItems;
-
+    public Timestamp snapshot;
+    
     public CartView() {
     }
 
     public CartView(Collection<CartItem> products, int totalItems) {
         this.products   = new ArrayList<>(products);
         this.totalItems = totalItems;
+        this.snapshot   = new Timestamp(System.currentTimeMillis());
     }
 }
